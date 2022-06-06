8c6eaea0212d2e1d6f6f0d5551078f0c014120fb
NekodaMushi <4252@holbertonschool.com>
2022-06-06 11:53:11 +0000

    Checker might work better than my terminal

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * * main - Most important part of the code
+ *
+ *  Return: 0 if succeed right
+ */
+int main(void)
+{
+	int h;
+	int d;
+	int u;
+	for (h = 48; d < 57; h++)
+	{
+		for (d = h + 1; d < 57; d++)
+		{
+			for (u = d + 1; u <= 57; u++)
+			{
+				putchar(h);
+				putchar(d);
+				putchar(u);
+
+				if (h == 55 && d == 56 && u == 57)
+				{
+					break;
+				}
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
