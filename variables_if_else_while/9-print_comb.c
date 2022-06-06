3cea179232abeb26d40145cccb15bdfb5fac77b6
NekodaMushi <4252@holbertonschool.com>
2022-06-06 10:19:34 +0000

    Using ASCII for the first time

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * * main - Most important part of the code
+ *
+ *  Return: 0 if succeeded
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n == 9)
+		{
+			continue;
+		}
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
