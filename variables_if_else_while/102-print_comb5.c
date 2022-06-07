d34f58044f1bf65f12d8afa6e8453bc58cf4c687
NekodaMushi <4252@holbertonschool.com>
2022-06-07 09:00:17 +0000

    using dividing and modulo to get 2 char in 1 var

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * * main - Most important part of the code
+ *
+ *  Return: 0 if succeed right
+ */
+int main(void)
+{
+	int a;
+	int b;
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+			if (a == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar(' ');
+	return (0);
+}
