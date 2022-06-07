066a3f346ed696e912dcf177c80b3b0f68809b54
NekodaMushi <4252@holbertonschool.com>
2022-06-07 12:19:42 +0000

    checking if character are lower, main.h updated for all projects

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - fct is printing alphabet 10x using _putchar
+ *
+ * Return: 0 if succeeded
+ */
+int _islower(int c)
+{
+	if (c > 96)
+		return (1);
+
+	return (0);
+}
