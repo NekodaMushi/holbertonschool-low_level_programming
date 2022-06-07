ec6cd3f0a7e6632d6601fa08168f1abeaa1c6d70
NekodaMushi <4252@holbertonschool.com>
2022-06-07 15:59:28 +0000

    Trying to do last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - initial fct
+ * @a: ASCII variable used by fct
+ * Return: 0 if succeeded
+ */
+
+int print_last_digit(int a)
+{
+	int last = a % 10;
+
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last);
+	return (last);
+}
