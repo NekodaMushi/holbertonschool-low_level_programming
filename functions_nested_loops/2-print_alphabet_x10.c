9a632680d1b4274925fbfbf2dc928b3eb92f1f39
NekodaMushi <4252@holbertonschool.com>
2022-06-07 11:44:03 +0000

    10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - fct is printing alphabet 10x using _putchar
+ *
+ * Return: 0 if succeeded
+ */
+void print_alphabet_x10(void)
+{
+	char ch, a;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (ch = 'a'; ch < 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
