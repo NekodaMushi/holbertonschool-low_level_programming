24f76795d68299fd98725ff7869e22caf6bd2686
NekodaMushi <4252@holbertonschool.com>
2022-06-07 10:04:38 +0000

    Nested Loop, using fct prototype

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Most important part of the code
+ *
+ * Return: 0 if succeeded
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
