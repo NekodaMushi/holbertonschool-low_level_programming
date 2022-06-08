79f8d433de33b9b6b1af28dbc0c9b6acee93ab74
NekodaMushi <4252@holbertonschool.com>
2022-06-08 09:33:31 +0000

    Pleasing betty

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * add - fct is printing multiplication table
- *
+ * @i: first integer
+ * @j: second interger additionned
  * Return: 0 if succeeded
  */
 int add(int i, int j)
