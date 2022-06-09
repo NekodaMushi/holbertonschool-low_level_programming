fb6afae21a92d2dab3b8e03f4293b60ea69483c4
NekodaMushi <4252@holbertonschool.com>
2022-06-09 13:08:52 +0000

    Condition if 100 added before the other, if i%15 became else if

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,10 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if (i == 100)
+			printf("Buzz");
+
+		else if (i % 15 == 0)
 			printf("FizzBuzz ");
 
 		else if (i % 5 == 0)
