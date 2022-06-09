9711609d75409e638cc4b6508dd508166defba74
NekodaMushi <4252@holbertonschool.com>
2022-06-09 08:32:01 +0000

    using ASCII added putchar

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	char ch;
+
+	for (ch = 48; ch <= 57; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
