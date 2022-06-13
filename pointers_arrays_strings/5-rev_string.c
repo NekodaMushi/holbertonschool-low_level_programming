37cf8f65a14ecbc8f1a469abbf436cf4be5225fd
NekodaMushi <4252@holbertonschool.com>
2022-06-13 13:33:35 +0000

    Using two fct

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - returns the length of a string.
+ * @s: var to be used
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int len, tmp;
+	int left = 0;
+	len = (strlen(s) - 1);
+
+	while (len > left)
+	{
+		tmp = s[left];
+		s[left] = s[len];
+		s[len] = tmp;
+		left++;
+		len--;
+	}
+}
+
+int _strlen(char *s)
+{
+	int strlen = 0;
+
+	while (s[strlen] != '\0')
+		strlen++;
+	return (strlen);
+}
