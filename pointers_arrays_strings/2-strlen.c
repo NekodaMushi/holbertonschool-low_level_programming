d86570dff6ec5d6fa02add1afd707a91af5e6eff
NekodaMushi <4252@holbertonschool.com>
2022-06-13 10:18:25 +0000

    using array

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  _strlen - returns the length of a string.
+ * @s: var to be used
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+		i++;
+	return (0);
+}
