beb82562ecc8662bbf6dfed706a8ecfacd6374f7
NekodaMushi <4252@holbertonschool.com>
2022-06-13 12:14:03 +0000

    --i at the end of first loop

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,11 +11,13 @@ void print_rev(char *s)
 	int i = 0;
 
 	while (s[i] != '\0')
+	{
 		i++;
-	while (i >= 0)
+	}
+	--i;
+	for (; i >= 0; i--)
 	{
 		_putchar(s[i]);
-		i--;
 	}
 	_putchar('\n');
 }
