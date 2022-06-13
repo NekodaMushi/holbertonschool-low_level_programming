a30a0dcaa701a7c2573a11da52bd96a4a1a4443f
NekodaMushi <4252@holbertonschool.com>
2022-06-13 09:39:39 +0000

    pointing to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  reset_to_98 - updates the value it points
+ * @n: var to be used
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
