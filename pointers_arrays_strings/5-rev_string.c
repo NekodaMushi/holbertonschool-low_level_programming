37d2bfa3d998f8b67b332a528bfc0c3d35cc4166
NekodaMushi <4252@holbertonschool.com>
2022-06-13 13:57:54 +0000

    Pleasing betty

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,8 @@ void rev_string(char *s)
 {
 	int len, tmp;
 	int left = 0;
-	len = (strlen(s) - 1);
+
+	len = strlen(s) - 1;
 
 	while (len > left)
 	{
@@ -22,6 +23,12 @@ void rev_string(char *s)
 		len--;
 	}
 }
+
+/**
+ *  _strlen - returns the length of a string.
+ * @s: var to be used
+ * Return: Always 0.
+ */
 
 int _strlen(char *s)
 {
