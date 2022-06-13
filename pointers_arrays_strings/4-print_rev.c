1b443b3b52c5276c69970a1ae73c0b964914c87e
NekodaMushi <4252@holbertonschool.com>
2022-06-13 11:44:54 +0000

    initial and let cursor do before decrementing

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev- returns the length of a string.
+ * @s: var to be used
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
