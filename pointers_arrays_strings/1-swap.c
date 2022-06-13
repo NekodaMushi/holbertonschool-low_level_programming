0f16d9a600eb63c1dfeb927e948db9a041d15d24
NekodaMushi <4252@holbertonschool.com>
2022-06-13 09:53:31 +0000

    swapping value using pointer

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  swap_int - swaps the values of two integers.
+ * @a: var to be used*
+ * @b: var to be swapped
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int a, b;
+
+	b = *b;
+	b = a;
+	a = *b;
+}
