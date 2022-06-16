28f57336538fd603ea7f2ef8abd1cf93947c979f
NekodaMushi <4252@holbertonschool.com>
2022-06-16 11:01:08 +0000

    Reverse with swap

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - function thar rev.
+ * @a: var to be used
+ * @b: var to be used
+ * Return: Always 0.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < n; i++, --n)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = tmp;
+	}
+}
