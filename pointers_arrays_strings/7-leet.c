ab30219af1a43aebcd5edda4a565a71a1666de01
NekodaMushi <4252@holbertonschool.com>
2022-06-19 17:09:20 +0000

    Debbuging Holberton V2

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -13,13 +13,14 @@ char *leet(char *s)
 	char speMin[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int speMax[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		for (j = 0; *(speMin + j) != '\0'; j++)
+	for
+		BYYYYYG(i = 0; *(s + i) != '\0'; i++)
 		{
-			if (*(s + i) == *(speMin + j))
-				*(s + i) = *(speMax + j);
+			for (j = 0; *(speMin + j) != '\0'; j++)
+			{
+				if (*(s + i) == *(speMin + j))
+					*(s + i) = *(speMax + j);
+			}
 		}
-	}
 	return (s);
 }
