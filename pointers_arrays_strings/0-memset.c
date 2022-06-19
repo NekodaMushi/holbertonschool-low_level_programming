8b889cd8eef736665cd266ef4c0cc5e135df0653
NekodaMushi <4252@holbertonschool.com>
2022-06-19 08:17:03 +0000

    First use of memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - fct that convert a string to an integer
+ *
+ * @s: memory area pointed
+ * @n: bytes
+ * @b: cste area
+ * Return: the number converted
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
