a460272275c9d1438ad6f7468cabb1419dfb4296
NekodaMushi <4252@holbertonschool.com>
2022-06-21 08:43:13 +0000

    Pleasing betty

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y.
+ * _pow_recursion - function that returns the value
+ *  of x raised to the power of y.
  *
  * @x: var to be used
  * @y: var multiplying of y
