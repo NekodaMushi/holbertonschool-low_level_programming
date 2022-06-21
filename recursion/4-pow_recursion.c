23c69f420ace97a6d3fa7bbb6db33141d21159b6
NekodaMushi <4252@holbertonschool.com>
2022-06-21 08:42:04 +0000

    Power of

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - function that returns the value of x raised to the power of y.
+ *
+ * @x: var to be used
+ * @y: var multiplying of y
+ *
+ * Return: fct
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
