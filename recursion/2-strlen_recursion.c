c2c73a7f82706f185aa8406f38070779c136e0c4
NekodaMushi <4252@holbertonschool.com>
2022-06-21 07:51:49 +0000

    return length of string using recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that returns the length of a string.
+ *
+ * @s: string used in recursion
+ *
+ * Return: fct
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return 1 + _strlen_recursion(s + 1);
+	else
+		return 0;
+}
