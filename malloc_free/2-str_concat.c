c8f7c4543faffc1a4f1658956e9df9afc4a804ec
NekodaMushi <4252@holbertonschool.com>
2022-06-23 16:18:42 +0000

    hard as..

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ *str_concat - concatenates two strings
+ *@s1: first string
+ *@s2: second string
+ *Return: s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *rtn = malloc(strlen(s1) + strlen(s2) + 1);
+	int p, q = 0;
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	if (rtn == NULL)
+	{
+		return (NULL);
+	}
+	for (p = 0; (rtn[q] = s1[p]) != '\0'; ++p, ++q)
+	{
+	}
+	for (p = 0; (rtn[q] = s2[p]) != '\0'; ++p, ++q)
+	{
+	}
+	return rtn;
+}
