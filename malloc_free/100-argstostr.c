3f336782f5daa38d157520310a495d3fd107d1fb
NekodaMushi <4252@holbertonschool.com>
2022-06-26 18:57:57 +0000

    simple is good

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *argstostr - function that concatenates all the arguments
+ * @ac: argc
+ * @av: argv
+ * Return: Always 0 if succeeded
+ **/
+
+char *argstostr(int ac, char **av)
+{
+
+	int i;
+
+	if (ac == 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		printf("%s\n", av[i]);
+
+	if (av == NULL)
+		return (NULL);
+
+	return (0);
+}
