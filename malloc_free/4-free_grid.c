623af233c652e714ca490dfc11895e80d267d86a
NekodaMushi <4252@holbertonschool.com>
2022-06-26 18:47:16 +0000

    Don't be greedy and free

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *free_grid -  function that frees a 2 dimensional grid
+ *grid: array to be freed
+ *@height: int size height
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
