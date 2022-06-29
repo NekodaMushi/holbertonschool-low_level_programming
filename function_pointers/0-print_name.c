f3a4b21445f06534bed685482c3b7323681d346f
NekodaMushi <4252@holbertonschool.com>
2022-06-29 19:48:20 +0000

    Printing name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name- function that prints a name
+ * @name: var to be printed
+ * @f: function who print name
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
