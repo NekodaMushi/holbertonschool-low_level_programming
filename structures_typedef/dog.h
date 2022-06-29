e681089172d5493a210de6d3d843ca9bf56956eb
NekodaMushi <4252@holbertonschool.com>
2022-06-29 11:50:26 +0000

    dog updated with prototypes

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,9 +13,12 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_file;
+} dog_t;
 
 /*Functions Prototype from this project:*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
