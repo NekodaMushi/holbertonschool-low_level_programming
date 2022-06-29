9f4d68fb476cea07b1f8b16037e42a1ebcb8e972
NekodaMushi <4252@holbertonschool.com>
2022-06-29 10:18:27 +0000

    First structure using a dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * DOG - structure defining dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog's owner
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_file;
+
+#endif
