c68337b460cd7f6a9467be65bb710db79d54ed19
NekodaMushi <4252@holbertonschool.com>
2022-06-30 08:16:58 +0000

    Compressing

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	long unsigned int i;
 
-	if (array != NULL && action != NULL)
+	if (array && action)
 	{
 		for (i = 0; i < size; i++)
 		{
