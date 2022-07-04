29d085a973fb2c2e153f03a9f0fe52533d1e1039
NekodaMushi <4252@holbertonschool.com>
2022-07-04 10:24:47 +0000

    separator

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers.
+ *
+ * @n: numbers of parameters.
+ * @separator: separator between numbers
+ *
+ * Return: Nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list(args);
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n)
+			printf("%d", va_arg(args, int));
+		if (separator != 0)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+}
