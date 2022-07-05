68b7562a7f3fede43817b73448627b9f602b62b6
NekodaMushi <4252@holbertonschool.com>
2022-07-05 20:26:58 +0000

    header updated

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,13 +4,30 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/*Functions Prototype from this project:*/
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char *const format, ...);
-
 /*Function created before*/
 int _putchar(char c);
+
+/*Functions Prototype from this project:*/
+int sum_them_all(const unsigned int n, ...);
+void print_all(const char *const format, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_char_pointer(va_list args);
+
+/**
+ * struct search_type - struct type
+ * @type: char's format
+ * @f: function pointer
+ *
+ */
+
+typedef struct search_type
+{
+	char *type;
+	void (*f)(va_list);
+} search_type_st;
 
 #endif
