97c2572cc3d60366e4464954127b5dd42278b5ee
NekodaMushi <4252@holbertonschool.com>
2022-07-13 14:32:17 +0000

    Run before main

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -6,5 +6,6 @@ void turtle(void) __attribute__((constructor));
  */
 void turtle(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
