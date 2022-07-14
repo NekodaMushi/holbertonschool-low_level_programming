c6ea2bf320010efe7c1bd1242d9d318ed69b34f2
NekodaMushi <4252@holbertonschool.com>
2022-07-14 14:06:54 +0000

    Love training on L.Lists

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_list - fct that prints elements of list
+ * @h: list address
+ * Return: number of elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int number_of_node = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		number_of_node++;
+		h = h->next;
+	}
+	return (number_of_node);
+}
