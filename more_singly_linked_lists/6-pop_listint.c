a3fec391bcb3e6796fddc10b278b5f8b586a1905
NekodaMushi <4252@holbertonschool.com>
2022-07-15 11:33:25 +0000

    Teasing checker

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - fct that erase node from start of list
+ * @head: head of the list
+ * Return: 0 if succeed
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	/**int value;*/
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	/**value = (*head)->n;*/
+	*head = (*head)->next;
+	free(tmp);
+
+	return ((*head)->n);
+}
