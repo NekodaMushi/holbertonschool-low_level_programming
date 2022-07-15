0918d419852bec816ea1a974e9f9f26114b3753b
NekodaMushi <4252@holbertonschool.com>
2022-07-15 10:23:10 +0000

    freeing pointer as well

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - fct that prints nb of elements from list
+ * @head: head of the list
+ * Return: number of elements
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
