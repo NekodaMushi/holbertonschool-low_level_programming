3cfd035ac05988b038bacf41bb0e7c0ef524b100
NekodaMushi <4252@holbertonschool.com>
2022-07-15 10:29:19 +0000

    freeing pointer as well

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,13 @@ void free_listint2(listint_t **head)
 	listint_t *tmp;
 
 	if (head == NULL || *head == NULL)
+		return;
 
-		while (*head != NULL)
-		{
-			tmp = *head;
-			*head = (*head)->next;
-			free(tmp);
-		}
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
 	*head = NULL;
 }
