af1161224fad9c1334dd516e11472b3558d8ad75
NekodaMushi <4252@holbertonschool.com>
2022-07-15 14:07:58 +0000

    Addressing value and pointer inside node right after allocation

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -11,13 +11,16 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *node;
-	listint_t *tmp, *tmp1;
+	listint_t *tmp;
 
 	unsigned int count;
 
 	node = malloc(sizeof(listint_t));
 	if (!node || !head)
 		return (NULL);
+
+	node->n = n;
+	node->next = NULL;
 
 	tmp = *head;
 
