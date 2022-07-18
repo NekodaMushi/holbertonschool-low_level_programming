6651fb34234c92c80f7255b3ed130b86840790d9
NekodaMushi <4252@holbertonschool.com>
2022-07-18 13:14:24 +0000

    Segmentation fault corrected

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int slen = strlen(b);
+	int slen = 0;
 	int result = 0;
 	int decval = 1;
 	int i;
 
-	for (i = (slen - 1); i >= 0; i--)
+	if (b != NULL)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		if (b[i] == '1')
-			result += decval;
-		decval *= 2;
+		slen = strlen(b);
+		for (i = (slen - 1); i >= 0; i--)
+		{
+			if (b[i] != '0' && b[i] != '1')
+				return (0);
+			if (b[i] == '1')
+				result += decval;
+			decval *= 2;
+		}
 	}
-	return result;
+	return (result);
 }
