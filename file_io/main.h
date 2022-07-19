a9d506047d4536a0585f3229d6878f1c35c3c562
NekodaMushi <4252@holbertonschool.com>
2022-07-19 12:18:08 +0000

    Main Readme Updated

---
+++ file_io/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stddef.h>
+#include <fcntl.h>
+
+/*Function created before*/
+int _putchar(char c);
+int _strlen(const char *str);
+
+/*Functions Prototype from this project:*/
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
