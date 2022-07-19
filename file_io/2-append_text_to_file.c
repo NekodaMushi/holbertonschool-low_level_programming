46f71b85b394a6287eeb995f0535d2d5936ad726
NekodaMushi <4252@holbertonschool.com>
2022-07-19 12:55:32 +0000

    Append text to a file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - fct that appends text to a file
+ *
+ * @filename: name of the file
+ * @text_content: Content of file created
+ * Return: 0
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, write_file;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	write_file = write(fd, text_content, strlen(text_content));
+	if (write_file == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
