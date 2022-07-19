89a3067e35a4da2207e60536021d65682ce7de35
NekodaMushi <4252@holbertonschool.com>
2022-07-19 12:14:27 +0000

    read copy and paste in new file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - fct that read a file's content
+ *  and create a new file with this content
+ *
+ * @filename: name of the file
+ * @letters: var used
+ * @return ssize_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, read_, write_;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(ssize_t) * letters);
+	if (buf == NULL)
+	{
+		free(buf);
+		return (0);
+	}
+
+	read_ = read(fd, buf, letters);
+	if (read_ == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	write_ = write(STDOUT_FILENO, buf, read_);
+	if (write_ == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	close(fd);
+	free(buf);
+	return (write_);
+}
