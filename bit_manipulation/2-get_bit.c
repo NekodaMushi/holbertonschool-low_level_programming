111bb97ef1e01137de4229ff6af31d019b86e0e1
NekodaMushi <4252@holbertonschool.com>
2022-07-20 09:01:45 +0000

    File added

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - Convert binary to unsigned int
+ * @n: binary number
+ * @index: Position of bit
+ * Return: Value of bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 15)
+		return (-1);
+	return ((n >> index) & 1);
+}
