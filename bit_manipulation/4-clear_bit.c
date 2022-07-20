4e6a9f4860ad853bd5a1ecc7d6a24af53e542ab6
NekodaMushi <4252@holbertonschool.com>
2022-07-20 10:48:53 +0000

    Pleasing betty and set 1 to 0 with new task

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * set_bit - Set bit from 0 to 1 at given index
+ * clear_bit - Convert binary to unsigned int
  * @n: binary number
  * @index: Position of bit
  * Return: Value of bit at index
  */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index <= 32)
 	{
-		*n |= (1 << index);
+		*n &= ~(1 << index);
 		return (1);
 	}
 	else
