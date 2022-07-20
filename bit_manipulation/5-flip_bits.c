e9f86887e26ddcfab9c5377821b16f6f52933fa5
NekodaMushi <4252@holbertonschool.com>
2022-07-20 12:27:28 +0000

    Difference

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the nb of bits you would need to flip
+ * @n: binary number
+ * @m: Position of bit
+ * Return: Value of bit at index
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count = 0;
+	unsigned int t;
+
+	t = n ^ m;
+
+	while (t > 0)
+	{
+		count += (t & 1);
+		t >>= 1;
+	}
+	return (count);
+}
