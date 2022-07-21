f375d037e29f3568f380b25f387ac35510ba6827
NekodaMushi <4252@holbertonschool.com>
2022-07-21 20:43:01 +0000

    Deleting should be easier with double LL

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - fct that delete a node at given index
+ * @head: head of the list
+ * @index: referencial
+ * Return: 1 if succeed and -1 if not.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp, *node;
+	unsigned int count;
+
+	if (*head == NULL)
+		return (-1);
+
+	tmp = *head;
+
+	if (index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+
+	for (count = 0; tmp && count < index - 1; count++)
+	{
+		if (!tmp || !tmp->next)
+			return (-1);
+		tmp = tmp->next;
+	}
+	node = tmp->next;
+	tmp->next = node->next;
+	tmp = tmp->next;
+	tmp->prev = node->prev;
+
+	free(node);
+
+	return (1);
+}
