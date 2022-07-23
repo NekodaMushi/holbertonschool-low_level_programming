be1af1f23a59ee1bf62d56fcd85cbb0836085955
NekodaMushi <4252@holbertonschool.com>
2022-07-23 07:05:53 +0000

    Trying to fix score from V2 bug

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -14,37 +14,3 @@ size_t print_listint_safe(const listint_t *head)
 	tmp = head;
 
 	while (head != NULL && loop_checker(tmp, head, number_of_node))
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		number_of_node++;
-		head = head->next;
-	}
-	if (head != NULL)
-	{
-		printf("-> [%p] %d\n", (void *)head, head->n);
-	}
-	return (number_of_node);
-}
-
-/**
- *loop_checker - fct that check where the loop is starting
- *@head: head of the list
- *@current: location to check where the loop start
- *@idx: referential used to find the loop
- *Return: 1 when loop found.
- */
-
-int loop_checker(const listint_t *head, const listint_t *current, size_t idx)
-{
-	size_t counter = 0;
-
-	while (head != current)
-	{
-		head = head->next;
-		counter++;
-	}
-	if (counter == idx)
-		return (1);
-
-	return (0);
-}
