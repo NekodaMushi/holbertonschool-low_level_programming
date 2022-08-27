015c30c350c59e2e5e9f28bb0a0263b6b2cc4bc9
NekodaMushi <4252@holbertonschool.com>
2022-08-27 07:49:25 +0000

    Setting up project's folder

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function that searches for a value in an array
+ *
+ * @array: pointer to first element of the array
+ * @size: nb of elements in array
+ * @value: value to search for
+ *
+ * Return: 0 if succeed or -1 if fails
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return i;
+	}
+	return (-1);
+}
